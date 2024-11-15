fc8236aa86c7da2e59aa9016fd860b2e9977787f
Lojitao <Lojitao@outlook.com>
2024-11-15 19:53:28 +0800

    TS:infer功能

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -35,49 +35,75 @@ function handleRequest(status: RequestStatus) {
       console.log("Unknown status");
   }
 }
-console.log(RequestStatus);
 
+type SetOptional<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+} & {
+  [P in K]?: T[P];
+};
 
-handleRequest(RequestStatus.Pending)
-
-
-
-
-
-type Status = number | string
-type Gender = '男'|'女'
-type Area = {
-  height:number,
-  width:number
-}
-type Address = {
-  num:number,
-  cell:number,
-  room:string,
-  haha?:string
+interface Foo {
+  a: number;
+  b?: string;
+  c: boolean;
 }
 
-type House = Area & Address
-const house:House = {
-  height:10,
-  width:20,
-  num:20,
-  cell:33,
-  room:'高級房',
+type SomeOptional = SetOptional<Foo, 'a' | 'b'>;
+
+let newObj :SomeOptional={
+  c:true
 }
 
 
+// 自定义工具类型，用于判断两个类型是否完全相等
+type IsEqual<A, B> = (
+  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true : false
+);
 
 
 
-enum Direction {
-  UP ='UP',
-  DOWN = 'DOWN',
-  RIGHT = 'RIGHT',
-  LEFT = 'LEFT'
+
+
+
+interface Example {
+  a: string;
+  e: number;
+  b: string | number;
+  c: () => void;
+  d: {};
+  f: string | number | boolean;
 }
 
- 
+
+type ConditionalPick<T,Condition> = {
+  //部分符合即可
+  [K in keyof T as [Extract<T[K], Condition>] extends [never] ? never : K]: T[K];
+
+  //完全一樣
+  // [K in keyof T as IsEqual<T[K], Condition> extends true ? K : never]: T[K];
+}
+
+type StringKeysOnly = ConditionalPick<Example, string | number>;
+
+let result:StringKeysOnly = {
+  b:'asd',
+  a: 'string',
+  e: 343,
+  f:'adas'
+}
+
+
+//TS 练习题第五题:
+//定义一个工具类型 AppendArgument，为已有的函数类型增加指定类型的参数，新增的参数名是 x，将作为新函数类型的第一个参数。具体的使用示例如下所示：
+
+type Fn = (a: number, b: string) => number
+type AppendArgument<F, A> = F extends (...arg:infer Args)=>infer R
+  ?(x:A,...arg:Args)=>R
+  :never
+
+type FinalFn = AppendArgument<Fn, string> 
+// (x: boolean, a: number, b: string) => number
+
 
 
 
